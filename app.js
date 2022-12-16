e0f611481d7d947bb5932970bf932fb7b5bcf273
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-12-16 01:06:13 -0300

    feat: round temp with math.ceil function

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const https = require("https");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -22,7 +24,7 @@ app.post("/", (req, res) => {
 
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
-      const temp = weatherData.main.temp;
+      const temp = Math.ceil(weatherData.main.temp);
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
       const iconUrl = 'http://openweathermap.org/img/wn/'+icon +'@2x.png'
