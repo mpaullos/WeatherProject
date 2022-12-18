1e26562a31fbfee0270a33828989c6cb9e2385b9
Marcos Paulo <82289818+mpaullos@users.noreply.github.com>
2022-12-17 21:38:51 -0300

    feat: add ejs and path to render backend to frontend

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const https = require("https");
 
 const app = express();
 
+const path = require("path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,9 +21,10 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   const query = req.body.cityName;
-  const apiKey = '19ac5ae8e9670af8d804107c8bb7252d'
-  const unity = 'metric'
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=" +query + "&appid=" +apiKey + "&units=" +  unity+ "&lang=pt_br";
+  const apiKey = "19ac5ae8e9670af8d804107c8bb7252d";
+  const unity = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unity + "&lang=pt_br";
+
   https.get(url, (response) => {
     console.log(response.statusCode);
 
@@ -27,13 +33,21 @@ app.post("/", (req, res) => {
       const temp = Math.ceil(weatherData.main.temp);
       const weatherDescription = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      const iconUrl = 'http://openweathermap.org/img/wn/'+icon +'@2x.png'
+      const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
-      const text1 =`<h1>A temperatura em ${query} é ${temp} ${weatherDescription}</h1>`;
-      const text2 ='<img src=' + iconUrl  +'>'
-      res.send(text1 + text2)
-      
-      
+      var day = new Date();
+      var hours = day.getHours();
+      var minutes = day.getMinutes();
+      function fixHourAndMinutes(hours) {
+        if (hours < 10){
+          hours = "0" + hours;
+        }
+        return hours;
+      }
+
+      //const text1 = `<h1>A temperatura em ${query} é ${temp} ${weatherDescription}</h1>`;
+      // const text2 = "<img src=" + iconUrl + ">";
+      res.render("index", { h1: temp, h2: query, h3: `${fixHourAndMinutes(hours)}:${fixHourAndMinutes(minutes)}` });
     });
   });
 });
